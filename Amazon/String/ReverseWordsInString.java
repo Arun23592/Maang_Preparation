8113e2b602d5ef310eaa5f3199a7c4a1d1a9fd15
Arun23592 <arunsubramani1992@gmail.com>
2024-12-03 23:00:39 +0530

    ReverseWords

---
+++ Amazon/String/ReverseWordsInString.java
@@ -0,0 +1,24 @@
+package Maang_Preparation.Amazon.String;
+
+public class ReverseWordsInString {
+
+  public String reverseWords(String str){
+    String[] s = str.trim().split("\\s+");
+    String rev = "";
+    for(int i = s.length-1; i>=0; i--){
+        rev += s[i] + " ";
+    }
+
+    return rev + s[0];
+  }
+
+
+  public static void main(String[] args) {
+    String str = "the sky is blue";
+    ReverseWordsInString revWords = new ReverseWordsInString();
+     String result = revWords.reverseWords(str);
+     System.out.println("Reverse String:"+result);
+    
+  }
+
+}
