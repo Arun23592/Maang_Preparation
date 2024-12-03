8b1f3ddbbe34d79b82181d86673bf99a437592fe
Arun23592 <arunsubramani1992@gmail.com>
2024-12-03 21:29:47 +0530

    ReverseString

---
+++ Amazon/String/ReverseString.java
@@ -0,0 +1,31 @@
+package Maang_Preparation.Amazon.String;
+
+public class ReverseString {
+
+  public void reverseString(char[] s){
+    
+    int left = 0;
+    int right = s.length - 1;
+
+    while (left < right) {
+      char temp = s[left];
+      s[left] = s[right];
+      s[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+
+  public static void main(String[] args) {
+    String s = "Hannah";
+    char[] chars = s.toCharArray();
+    ReverseString rev = new ReverseString();
+
+    rev.reverseString(chars);
+    String result = new String(chars);
+    System.out.println("Reversed String: "+result);
+
+  }
+
+}
