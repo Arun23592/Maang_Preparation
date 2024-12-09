8c6e96bcb17bda63eac2897f896213c45f322314
Arun23592 <arunsubramani1992@gmail.com>
2024-12-09 23:17:17 +0530

    long

--- Walmart/LengthOfLongestSubstring.java
+++ Walmart/LengthOfLongestSubstring.java
@@ -3,7 +3,7 @@ package Maang_Preparation.Walmart;
 import java.util.HashMap;
 import java.util.Map;
 
-import Maang_Preparation.Amazon.String.LongestSubstring;
+
 
 public class LengthOfLongestSubstring {
 
@@ -23,7 +23,7 @@ public class LengthOfLongestSubstring {
       }
 
       return maximumLength;
-
+    }
   public static void main(String[] args) {
       String s = "pwwkew";
       LengthOfLongestSubstring subString = new LengthOfLongestSubstring();
@@ -32,3 +32,52 @@ public class LengthOfLongestSubstring {
     }
 
 }
+
+
+
+/*
+ * Approach: This a classic sliding window problem where we use two pointers(left and right) to maintain a window of characters with no duplicates.
+ *  
+ * Time complexity: O(n) -> Each character is processed at most twice (once by right and possibly once by left), making the time complexity linear with respect to the length of the input string.
+ * Space complexity: O(min(n, m)) -> In the worst case, we might need to store all characters in
+ * the map, where n is the length of the string and m is the size of the character set
+ * 
+ * Explanation:
+ * 1. Initialize maximumLength and HashMap
+ * 2. Sliding window
+ * 3. Return result
+ * 
+ * Test case:
+ * 1. basic test:
+ * input: "pwwkew"
+ * output: 3
+ * 2. All unique characters
+ * input: "abcdef"
+ * output: 6
+ * 3. No unique characters
+ * input: "aaaaaa"
+ * output: 1
+ * 4. Mixed characters
+ * input: "abcabcbb"
+ * output: 3
+ * 5. String with special characters
+ * Input: "a!b@c#d$"
+ * Output: 8
+ * 
+ * Edge cases:
+ * 1. Empty String
+ * input: ""
+ * output: 0
+ * 2. Single character
+ * input: "a"
+ * output: 1
+ * 3. String with spaces
+ * input: "abc abc"
+ * output: 4
+ * 4. String with numbers and characters
+ * input: "abc123abc"
+ * output: 6
+ * 
+ * 
+ * 
+ */
