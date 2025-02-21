a9841072f951444c926ae06d06d13b64cd323359
Arun23592 <arunsubramani1992@gmail.com>
2025-02-21 22:42:41 +0530

    commonprobs

--- CommonProbs/Pattern.java
+++ CommonProbs/Pattern.java
@@ -4,9 +4,9 @@ public class Pattern {
 
   public static void main(String[] args){
     int n = 10;
-    for(int i=1; i<=n; i++){
+    for(int i=0; i<=n; i++){
       for(int j=1; j<=i; j++){
-        System.out.print("* ");
+        System.out.print("*");
       }
       System.out.println();
     }
