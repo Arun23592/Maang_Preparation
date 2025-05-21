df1033f7bebe31839b6addeb6147b0f98daa85ba
arunsubramani <arunsubramani1992@gmail.com>
2025-05-21 23:53:51 +0530

    dup

--- Amazon/Arrays/FindDuplicatesArray.java
+++ Amazon/Arrays/FindDuplicatesArray.java
@@ -26,7 +26,7 @@ public class FindDuplicatesArray {
 
   public static void main(String[] args) {
     int[] arr = {1, 2, 2, 4, 5, 5, 3};
-    duplicateArrays(arr);
+    int[] dup = duplicateArrays(arr);
     
 
   }
