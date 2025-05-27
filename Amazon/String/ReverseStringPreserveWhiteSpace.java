9a4cdcae7a4f3c054423e15f3cd26e79c087277b
arunsubramani <arunsubramani1992@gmail.com>
2025-05-27 22:02:25 +0530

    reversestringspacepreserved

---
+++ Amazon/String/ReverseStringPreserveWhiteSpace.java
@@ -0,0 +1,35 @@
+package Maang_Preparation.Amazon.String;
+
+public class ReverseStringPreserveWhiteSpace {
+
+    public static String reversePreserveWithWhiteSpace(String input){
+        char[] inputArr = input.toCharArray();
+        char[] result = new char[inputArr.length];
+
+        for(int i = 0; i<inputArr.length; i++){
+            if(Character.isWhitespace(inputArr[i])){
+                result[i] = inputArr[i];
+            }
+        }
+
+        int j = inputArr.length - 1;
+        for(int i=0; i<inputArr.length; i++){
+            if(!Character.isWhitespace(inputArr[i])){
+                while (j >= 0 && Character.isWhitespace(inputArr[j])) {
+                    j--;
+                }
+                result[i] = inputArr[j];
+                j--;
+            }
+        }
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+        String input = "a b c d e";
+        String output = reversePreserveWithWhiteSpace(input);
+        System.out.println("Original: \""+ input + "\"");
+        System.out.println("Original: \""+ output + "\"");
+    }
+
+}
