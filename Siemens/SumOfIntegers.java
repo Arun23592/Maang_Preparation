223a59b046e817d4f4b03fe13066f0fb9288b427
arunsubramani <arunsubramani1992@gmail.com>
2025-06-25 23:10:05 +0530

    sumofintegers

--- Siemens/SumOfIntegers.java
+++ Siemens/SumOfIntegers.java
@@ -5,7 +5,7 @@ import Collections.Set.set;
 public class SumOfIntegers {
 
     public static void main(String[] args){
-      int n = 100;
+      int n = 5;
       int sum = 0;
 
       for(int i=1; i<=n; i++){
