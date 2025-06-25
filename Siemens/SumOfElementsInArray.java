e2cbe0b14cb023a31c544c4ecfce3c4afa32c962
arunsubramani <arunsubramani1992@gmail.com>
2025-06-25 23:07:20 +0530

    SumofElements

--- Siemens/SumOfElementsInArray.java
+++ Siemens/SumOfElementsInArray.java
@@ -4,10 +4,10 @@ public class SumOfElementsInArray {
 
   public static void main(String[] args) {
     int[] array = {2, 4, 6, 8};
-    int sum = 1;
+    int sum = 0;
 
     for(int num: array){
-      sum *= num;
+      sum += num;
     }
 
     System.out.println(sum);
