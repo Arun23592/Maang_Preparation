3b472f4448c374c86ffc2dd6c6a0f318e2928956
arunsubramani <arunsubramani1992@gmail.com>
2025-06-29 23:48:57 +0530

    Hashmap

--- Collections/Map/Hashmap.java
+++ Collections/Map/Hashmap.java
@@ -1,5 +1,35 @@
 package Collections.Map;
+import java.util.HashMap;
 
 public class Hashmap {
+
+  public static void main(String[] args) {
+
+    //create a Hashmap
+    HashMap<Integer, String> map = new HashMap<>();
+
+    //Add key-value pairs
+    map.put(101, "Arun");
+    map.put(102, "priya");
+    map.put(104, "Kiran");
+
+    //Access value by key
+    System.out.println("Name with Id :"+ map.get(102));
+
+    //Iterate through HashMap
+    for(Integer key: map.keySet()){
+      System.out.println("key " + key + ", Value: "+ map.get(key));
+    }
+
+    //Remove an entry
+    map.remove(101);
+
+    System.out.println("After removal: "+ map);
+    
+  }
+  
+
+  
+  
 
 }
