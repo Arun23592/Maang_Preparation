69ff29cdb3fd95a00d169d7b7873cb5dd6164238
arunsubramani <arunsubramani1992@gmail.com>
2025-06-30 23:28:33 +0530

    LinkedList

--- Collections/List/LinkedList.java
+++ Collections/List/LinkedList.java
@@ -1,18 +1,30 @@
-import java.io.*;
-import java.util.List;
-import java.util.LinkedList;
 
 public class LinkedList {
 
     public static void main(String[] args) {
-        
-        int n = 6;
+    
 
-        LinkedList<String> ll = new LinkedList<>();
+        java.util.LinkedList<String> fruits = new java.util.LinkedList<>();
 
-        for(int i = 1; i<= n ; i++){
+        fruits.add("Apple");
+        fruits.add("Blueberry");
+        fruits.add("Goa");
+        fruits.addFirst("Pomogranate");
+        fruits.addLast("Orange");
+        fruits.addLast("Mango");
+
+        //Display list
+        System.out.println("Fruits Lists: "+fruits);
+
+        for(String fruit: fruits){
+            System.out.println("- "+fruit);
+            if (fruit == "apple") {
+                fruits.remove();
+            
+            }
             
         }
+
     }
 
 }
