dd0a29e26cf1364ad8e09e9e94dff403580b6668
arunsubramani <arunsubramani1992@gmail.com>
2025-07-10 23:38:58 +0530

    string

--- CommonProbs/PalindromeString.java
+++ CommonProbs/PalindromeString.java
@@ -19,7 +19,7 @@ public class PalindromeString {
     return true;
   }
     public static void main(String[] args) {
-      String s = "Madam";
+      String s = "Madams";
       System.out.println("\nIs this palindrome: "+panlindromes(s));
       
 
