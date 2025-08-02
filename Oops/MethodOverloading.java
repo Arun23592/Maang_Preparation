5a55db6218eb50b737fb09937b8f7dce819c97e5
arunsubramani <arunsubramani1992@gmail.com>
2025-08-02 23:19:03 +0530

    MO2

--- Oops/MethodOverloading.java
+++ Oops/MethodOverloading.java
@@ -13,3 +13,41 @@ public class MethodOverloading {
       return a + b + c;
     }
 }
+
+
+/*
+ * Another example:
+ * 
+ * public class LoginPage {
+
+    // Login with username & password
+    public void login(String username, String password) {
+        System.out.println("Login with username and password");
+    }
+
+    // Overloaded: Login with username, password & OTP
+    public void login(String username, String password, int otp) {
+        System.out.println("Login with username, password, and OTP");
+    }
+    
+    // Overloaded: Login with email
+    public void login(String email) {
+        System.out.println("Login with email only");
+    }
+}
+
+
+ *
+ * 
+ * ✅ Where to Use in Automation Framework?
+Example Use Cases:
+
+Page class methods for different variations of the same action.
+
+click(), click(String locator), click(By locator) in utility/helper classes.
+
+Overloading waitForElement() with different parameters.
+ * 
+ *  
+ * 
+ */
