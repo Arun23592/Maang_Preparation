7dcd9c96da02aa2758846f6456bd380244156b86
arunsubramani <arunsubramani1992@gmail.com>
2025-08-12 23:32:32 +0530

    Remove

--- Amazon/String/RemoveDuplicates.java
+++ Amazon/String/RemoveDuplicates.java
@@ -1,5 +1,35 @@
 package Amazon.String;
 
+import java.util.LinkedHashSet;
+
 public class RemoveDuplicates {
+
+  public static String removeDuplicateString(String s){
+
+    //1. Create LinkedhashSet to store count
+    LinkedHashSet<Character> count = new LinkedHashSet<>();
+
+    for(char c : s.toCharArray()){
+      count.add(c);
+    }
+
+    //2. create StringBuilder to Build the resulting string
+    StringBuilder result = new StringBuilder();
+
+    for(char c: count){
+      result.append(c);
+    }
+
+    //3. return the result to the string
+    return result.toString();
+
+  }
+
+  public static void main(String[] args) {
+    String s = "Programming";
+
+    String outString = removeDuplicateString(s);
+    System.out.println("Duplicate strings are removed: "+outString);
+  }
 
 }
