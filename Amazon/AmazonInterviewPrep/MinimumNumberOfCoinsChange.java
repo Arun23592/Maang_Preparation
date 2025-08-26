16f3c3b88061c61f11f4930466ff33338f81383d
arunsubramani <arunsubramani1992@gmail.com>
2025-08-26 23:49:25 +0530

    MinimumCoinchange

---
+++ Amazon/AmazonInterviewPrep/MinimumNumberOfCoinsChange.java
@@ -0,0 +1,11 @@
+package Amazon.AmazonInterviewPrep;
+
+public class MinimumNumberOfCoinsChange {
+
+
+  public static void main(String[] args) {
+    int[] coins = {1, 2, 5};
+    int amount = 11;
+  }
+
+}
