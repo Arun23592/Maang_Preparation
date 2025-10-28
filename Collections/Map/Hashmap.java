a9280a30bc3a40b947e2f1dca584ace417c2905c
arunsubramani <arunsubramani1992@gmail.com>
2025-10-28 22:15:49 +0530

    HashMap

--- Collections/Map/Hashmap.java
+++ Collections/Map/Hashmap.java
@@ -33,3 +33,25 @@ public class Hashmap {
   
 
 }
+
+
+
+/*
+ * 
+ * HashMap is a key-value pair data structure in Java.
+It stores data based on hashing and does not maintain any order.
+
+🧩 Key Points:
+
+Allows one null key and multiple null values.
+
+Not synchronized → not thread-safe (used in single-threaded environments).
+
+Very fast for lookups and inserts (O(1) on average).
+
+interview:
+
+“I use HashMap frequently in automation to store dynamic test data, like mapping test case IDs to expected results.
+It’s ideal for single-threaded test executions due to its speed and flexibility.”
+ * 
+ */
