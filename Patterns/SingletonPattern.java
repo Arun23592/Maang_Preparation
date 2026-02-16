df3cd40315d5b22e803ec4a440e1b63c574d42bc
arunsubramani <arunsubramani1992@gmail.com>
2026-02-16 23:40:03 +0530

    singleton

--- Patterns/SingletonPattern.java
+++ Patterns/SingletonPattern.java
@@ -1,6 +1,29 @@
 package Patterns;
 
-public class SingletonPattern {
+class Singleton{
+   //1. instance of the class
+   private static final Singleton INSTANCE  = new Singleton();
 
-  private static di
+   //2. private constructor to protect/avoid intanstiation/object
+
+   public Singleton(){
+     System.out.println("Singleton Instance created");
+   }
+
+   //3. provide access to the method
+   public static Singleton getInstance(){
+    return INSTANCE;
+   }
+}
+
+public class SingletonPattern {
+ 
+  public static void main(String[] args) {
+    Singleton obj1 = new Singleton();
+    Singleton obj2 = new Singleton();
+
+
+    System.out.println(obj1 == obj2);
+  }
+
 }
