a328244ed90f501a9e06b5cfdd9abc2ca18b2e67
arunsubramani <arunsubramani1992@gmail.com>
2026-04-05 23:36:01 +0530

    findmissing

---
+++ Amazon/Arrays/FindMissingNumber.java
@@ -0,0 +1,23 @@
+package Amazon.Arrays;
+
+public class FindMissingNumber {
+
+  public static void main(String[] args) {
+    int[] nums = {9,6,4,2,3,5,7,0,1};
+
+    int n = nums.length;
+
+    int expectedSum = n * (n + 1) / 2;
+
+    int actualSum = 0;
+
+
+    for(int num: nums){
+      actualSum += num;
+    }
+
+    int totalSum = expectedSum - actualSum;
+
+    System.out.println(totalSum);
+  }
+}
