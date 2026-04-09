174ae9b6db440e86371401a714c482a5b3120690
arunsubramani <arunsubramani1992@gmail.com>
2026-04-09 22:29:25 +0530

    secondlargests

--- Amazon/Arrays/SecondLargestElement.java
+++ Amazon/Arrays/SecondLargestElement.java
@@ -3,7 +3,7 @@ package Amazon.Arrays;
 public class SecondLargestElement {
 
   public static void main(String[] args) {
-    int[] arr = {10, 20, 40, 60, 100};
+    int[] arr = {10, 20, 40, 60, 80, 100};
 
     int largest = 0;
     int second_largest = 0;
