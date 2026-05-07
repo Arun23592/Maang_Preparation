2a1a5ff7094ead077a81e073fe7c850348091ae3
arunsubramani <arunsubramani1992@gmail.com>
2026-05-07 22:45:15 +0530

    Nextper

--- Amazon/AmazonInterviewPrep/NextPermutations.java
+++ Amazon/AmazonInterviewPrep/NextPermutations.java
@@ -50,3 +50,11 @@ public class NextPermutations {
   }
 
 }
+
+
+/***
+ * 
+ * 
+ * 	Given a sequence, return its next lexicographically greater permutation. If such a
+permutation does not exist, then return it in ascending order (ie, the lowest possible order).
+ */
