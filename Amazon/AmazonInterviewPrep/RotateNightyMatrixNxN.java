e1c64d65a50cf176b926850886ef1619b0bab15b
arunsubramani <arunsubramani1992@gmail.com>
2026-06-03 23:30:32 +0530

    rotate

--- Amazon/AmazonInterviewPrep/RotateNightyMatrixNxN.java
+++ Amazon/AmazonInterviewPrep/RotateNightyMatrixNxN.java
@@ -29,10 +29,10 @@ public class RotateNightyMatrixNxN {
   }
 
   public static void main(String[] args) {
-    int[][] matrix = {{1, 2, 3},
-                      {4, 5, 6},
-                      {7, 8, 9}
-                    };
+      int[][] matrix = {{1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                      };
 
     rotateNineghtyMatrix(matrix);
 
