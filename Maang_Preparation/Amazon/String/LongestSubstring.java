a52bed803e2fb09e1e182ff0f05a7ac52e183ee1
arunsubramani <arunsubramani1992@gmail.com>
2026-09-11 23:18:21 +0530

    string

---
+++ Maang_Preparation/Amazon/String/LongestSubstring.java
@@ -0,0 +1,35 @@
+package Maang_Preparation.Amazon.String;
+
+import java.util.*;
+
+public class LongestSubstring {
+
+    public int lengthOfLongestSubstring(String s){
+      int maximumLength = 0;
+
+      Map<Character, Integer> visitedCharacters = new HashMap<>();
+
+      for(int left=0, right=0; right<s.length(); right++){
+        char currentCharacters = s.charAt(right);
+        if(visitedCharacters.containsKey(currentCharacters) &&
+        visitedCharacters.get(currentCharacters) >= left){
+          left = visitedCharacters.get(currentCharacters) + 1;
+        }
+        maximumLength = Math.max(maximumLength, right - left + 1);
+        visitedCharacters.put(currentCharacters, right);
+      }
+
+      return maximumLength;
+    }
+
+    public static void main(String[] args) {
+      String s = "pwwkew";
+      LongestSubstring subString = new LongestSubstring();
+      int result = subString.lengthOfLongestSubstring(s);
+      System.out.println("Longest subtring: "+result);
+    }
+}
+
+
+//1. Implemented Slding Window algorithm and two pointers technique
+//2. Initialize maximumLength and 
